5b41ad886fa254c1f7c884a7cbd2105229c5fc61
mydkong <contact.donkeykong@protonmail.com>
2026-02-28 13:01:42 +0100

    fix .env deletion by executing the command, causing the crash of the bot

--- src/utils/update.sh
+++ src/utils/update.sh
@@ -1,6 +1,6 @@
 echo -e "\e[47;30;1mBOT IS UPDATING AND RESTARTING\e[0m"
 echo -e "\e[47;30;1mDELETING ALL OF THE FILES\e[0m"
-rm -rf *
+find . -type f ! -name .env -exec rm -rf {} +
 echo -e "\e[47;30;1mCLONING FROM THE OFFICIAL GITHUB REPOSITORY\e[0m"
 git clone https://github.com/FairPlayTeam/discord-bot.
 echo -e "\e[47;30;1mINSTALLING THE DEPENDENCIES\e[0m"
